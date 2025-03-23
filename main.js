15931f20c007545d94befff87a734ada54cb6c57
pierosalmena <piero.salmena@gmail.com>
2025-03-23 22:37:25 +0100

    Implement and test isSunk() method in Ship class
    Added the isSunk() method to the Ship class to determine whether a ship
    is considered sunk based on its length and number of hits.
    
    Wrote dynamic unit tests to verify the correct behavior of both hit()
    and isSunk() methods. Confirmed that the hit counter increments
    properly and isSunk() returns true when the number of hits equals the
    ship's length.

--- main.js
+++ main.js
@@ -7,17 +7,24 @@ class Ship {
 
     hit(){
         this.hitTimes++
+        if(this.hitTimes >= this.hitTimes) return this.isSunk()
     }
 
     isSunk(){
-
+        this.sunk = true;
     }
 }
 
 class Gameboard {
-    constructor(){
 
+    // Gameboards should be able to place ships at specific coordinates by calling the ship factory or class.
+    constructor(){
+        // Gameboards should keep track of missed attacks so they can display them properly.
+        // Gameboards should be able to report whether or not all of their ships have been sunk.
     }
+
+    // Gameboards should have a receiveAttack function that takes a pair of coordinates, determines whether or not the attack hit a ship and then sends the ‘hit’ function to the correct ship, or records the coordinates of the missed shot.
+
 }
 
 class Player{
--- main.test.js
+++ main.test.js
@@ -2,10 +2,20 @@ import {Ship} from './main.js'
 
 test('hit method', () => {
     const ship = new Ship(3)
+    const beforeFirstHit = ship.hitTimes
     ship.hit()
-    expect(ship.hitTimes.toBe(1))
+    expect(ship.hitTimes).toBe(beforeFirstHit + 1)
+})
+
+test('hit method on sunk', () => {
+    const ship = new Ship(2)
+    ship.hitTimes = 2;
+    ship.hit()
+    expect(ship.sunk).toBe(true)
 })
 
 test('isSunk method', () => {
-    expect(isSunk().toBe(true))
+    const ship = new Ship(3)
+    ship.isSunk()
+    expect(ship.sunk).toBe(true)
 })
